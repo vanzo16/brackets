b69367c3f84da74db5ec50a6c132f6c03079adcc
vanzo16 <vanzo16@ya.ru>
2021-03-14 12:05:51 +0300

    add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function bracketsFunction(brackets){
+  let Num = 0;
+  for( let i = 0; i < brackets.length; i++ ){
+    if (brackets.charAt(i) == "(" ) {
+      Num += 1;
+    }else{
+      Num -= 1;
+    }
+    if (Num < 0){
+      return false
+    }
+  }
+  if (Num === 0){
+    return true
+  }else {
+    return false
+  }
 }
+
+console.log(bracketsFunction("()))"));
